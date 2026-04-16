b4cf28820d8a2b19a4adb2c220027a1831db1981
Justin Downer <justindowner121@gmail.com>
2026-04-15 21:19:56 -0400

    NFT & Vault sol files

---
+++ Contracts/NFT.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EngineeringNFT is ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+
+    // Task 1: Custom Collection Name and Symbol
+    constructor() ERC721("JustinEngineerCollection", "JEC") Ownable(msg.sender) {}
+
+    // Task 3: Mint function that takes the IPFS JSON link
+    function mintMembership(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri); 
+    }
+}
